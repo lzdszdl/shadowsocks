1ba408baedbae2ad8d38585f791ca0313d7d17e7
lzdszdl <136208872@qq.com>
2018-03-24 12:53:43 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -12,7 +12,7 @@ MU_SUFFIX = 'zhaoj.in'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
 
 WEBAPI_URL = 'https://zhaoj.in'
 WEBAPI_TOKEN = 'glzjin'
